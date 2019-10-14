35b7568ffffe462a85cc1d38cbd2826498746db0
Nicole Adams <nicoleadams.boston@gmail.com>
2019-10-14 15:25:10 -0400

    Created a fresh project, added the Router

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
